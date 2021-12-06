bf57d70e26428b8f73f2248c413147a88ea06478
Martins <martins.kruklis@gmail.com>
2021-12-06 15:54:59 +0200

    solved exercise 1

--- src/01-fizz-buzz/index.ts
+++ src/01-fizz-buzz/index.ts
@@ -16,18 +16,20 @@
  * console.log('buzz')
  */
 
-function fizzBuzz(n: number) {
-    for (let a = 1; a < n; a++) {
-        if (a % 3 == 0 && a != 0) {
+function fizzBuzz(n: number ) {
+    for (let a = 1; a <= n; a++) {
+        if (a % 3 == 0 && a % 5 != 0 && a != 0) {
             console.log("fizz");
-        } else if (a % 5 == 0 && a != 0) {
+        } else if (a % 5 == 0 && a % 3 != 0 && a != 0) {
             console.log("buzz");
-        } else if (a % 3*5 == 0 && a != 0) {
+        } else if (a % 3 == 0 && a % 5 == 0 && a != 0) {
             console.log("fizzbuzz");
         } else {
             console.log(a);
         }
     }
 }
+
+console.log(fizzBuzz(5));
 
 export { fizzBuzz };
