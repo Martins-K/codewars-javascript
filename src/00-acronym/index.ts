18839b836cc98dd6e0e106466e05aa3483c0c685
Martins <martins.kruklis@gmail.com>
2021-12-06 16:20:20 +0200

    solved exercise 0 partially

--- src/00-acronym/index.ts
+++ src/00-acronym/index.ts
@@ -6,8 +6,10 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {
-    let stringArray = input.split(" ");
+function parse(input: string ) {
+    let arr = input.replace(/[^\w\s]/gi, '').split(" ");
+    let stringArray = arr.filter(el => {return el != ""});
+    console.log(stringArray);
     let acronym = [];
     for (let a = 0; a < stringArray.length; a++) {
         if (stringArray[a][0] == "_") {
