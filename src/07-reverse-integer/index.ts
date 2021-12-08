0e1a953f13910d86a838668d5675576b6a7df35a
Martins <martins.kruklis@gmail.com>
2021-12-08 14:32:21 +0200

    solved exercise 7

--- src/07-reverse-integer/index.ts
+++ src/07-reverse-integer/index.ts
@@ -13,9 +13,24 @@
  */
 
 function reverse(int: number) {
-    // let arr = int.toString().replace("0", "").split("");
-    let a = 500;
-    console.log(`500 reversed is ${a.toString().replaceAll("0", "").split("")}`);
+    let intToString = int.toString();
+    console.log("Parsing int to string...")
+    if (intToString[0] == "-") {
+        intToString = intToString.substring(1);
+        console.log("Removing minus sign...")
+    }
+    console.log("Stripping off minus sign...")
+    intToString = intToString.replace(/[^123456789]+/, "");
+    console.log("Deleting 0s from the end if the string...")
+    let reversedString = intToString.split("").reverse().join("");
+    console.log("Reversing string...")
+    if (int < 0) {
+        reversedString = "-" + reversedString;
+        console.log("Adding back minus sign...")
+    }
+    let reversedInt = parseInt(reversedString);
+    console.log(`Parsing string to int...`);
+    return reversedInt;
 }
 
 export { reverse };
