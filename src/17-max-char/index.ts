739f6d9d8a34b49ba077f680cca792823deb9add
Martins <martins.kruklis@gmail.com>
2021-12-12 17:14:49 +0200

    solved exercise 17 partially

--- src/17-max-char/index.ts
+++ src/17-max-char/index.ts
@@ -9,6 +9,18 @@
  * maxChar("apple 1231111") === "1"
  */
 
-function maxChar(str: string) {}
+function maxChar(str: string) {
+    let count : number = 0; 
+    let char : string = "";
+    console.log(`string is ${str}, length of the string is ${str.length}`);
+    for (let index = 0; index < str.length; index++) {
+    console.log(`There are ${str.replace(str[index], "").length} instances of character "${str[index]}"`);
+        if (str.length - str.replace(str[index], "").length > count) {
+            char = str[index];
+            count = str.length - str.replace(str[index], "").length;
+        }
+    }
+    return char;
+}
 
 export { maxChar };
