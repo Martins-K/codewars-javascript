9a64dedc41550b22fbb7c7a3663035b330a08cde
Martins <martins.kruklis@gmail.com>
2021-12-12 18:07:52 +0200

    solved exercise 18 partially

--- src/18-pyramid/index.ts
+++ src/18-pyramid/index.ts
@@ -16,6 +16,19 @@
  *              '#####'
  */
 
-function pyramid(n: number) {}
+function pyramid(n: number) {
+    if (n > 0) {
+        let counter = n - 1;
+        let arr: any[][] = [];
+        arr[0][0] = " ".repeat(counter) + "#".repeat(n * 2 - 1) + " ".repeat(counter);
+        console.log(arr[0][0]);
+        for (let index = 0; index < n; index++) {
+            arr[index].push(" ".repeat(counter) + "#".repeat(n * 2 - 1) + " ".repeat(counter))
+        }
+        return arr;
+    } else {
+        throw new Error("N must be a positive number");
+    }
+}
 
 export { pyramid };
