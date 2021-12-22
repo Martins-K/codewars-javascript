5c1401b7058dcf8a2cd0994a0882bc0bc9902171
Martins <martins.kruklis@gmail.com>
2021-12-22 19:28:42 +0200

    solved exercise 17

--- src/17-max-char/index.ts
+++ src/17-max-char/index.ts
@@ -10,30 +10,14 @@
  */
 
 function maxChar(str: string) {
-
-    const charMap = {};
-    let max = 0;
-    let maxChar = '';
-
-  // create character map
-  for (let char of str) {
-    if (charMap[char]) {
-      // increment the character's value if the character existed in the map
-      charMap[char]++;
-    } else {
-      // Otherwise, the value of the character will be increamented by 1
-      charMap[char] = 1;
-    }
-  }
-
-  // find the most commonly used character
-  for (let char in charMap) {
-    if (charMap[char] > max) {
-      max = charMap[char];
+  let max = 0;
+  let maxChar = '';
+  str.split('').forEach((char) => {
+    if (str.split(char).length - 1 > max) {
+      max = str.split(char).length - 1;
       maxChar = char;
     }
-  }
-
+  });
   return maxChar;
 }
 
